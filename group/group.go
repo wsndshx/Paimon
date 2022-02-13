2a55de30d93030b1047357a196adc47c68cb7f01
zzlwd <i@fishfish.date>
2022-02-13 11:06:23 +0800

    优化早晚安响应规则;

--- group/group.go
+++ group/group.go
@@ -12,9 +12,9 @@ func init() {
 
 func Handle(message string, num int64) {
 	// 匹配早安消息
-	morning := regexp.MustCompile(`^(早安|早上好|早鸭)|(早安|早上好|早鸭)$|早……`)
+	morning := regexp.MustCompile(`^早+|蒙早.{0,10}$`)
 	// 匹配晚安消息
-	night := regexp.MustCompile(`^(晚安|我睡了|睡)|(晚安|我睡了)`)
+	night := regexp.MustCompile(`^晚安|蒙晚安|睡.{0,1}了`)
 	if morning.MatchString(message) {
 		log.Println("消息`" + message + "`为早安问候")
 		greeting(1, num)
